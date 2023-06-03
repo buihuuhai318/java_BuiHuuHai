4f6ec1ae9c9f915f2227613eca36fe8b4ea7c938
buihuuhai318 <buihuuhai318@gmail.com>
2023-06-03 19:37:33 +0700

    Update Main.java

--- practice/is-prime/src/Main.java
+++ practice/is-prime/src/Main.java
@@ -5,12 +5,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number:");
         int number = scanner.nextInt();
-        
+
         if (number < 2){
             System.out.println(number + " is not a prime");
         } else {
             int i = 2;
             boolean check = true;
+
             while (i <= Math.sqrt(number)) {
                 if (number % i == 0) {
                     check = false;
@@ -18,6 +19,7 @@ public class Main {
                 }
                 i++;
             }
+            
             if (check)
                 System.out.println(number + " is a prime");
             else
