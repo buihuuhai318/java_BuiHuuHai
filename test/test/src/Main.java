1acd5cbac9b5f62775a74bcbbdd396be285a47aa
buihuuhai318 <buihuuhai318@gmail.com>
2023-06-05 08:38:54 +0700

    new

---
+++ test/test/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 100;
+        m(a);
+        System.out.println(a);
+    }
+
+    static void m(int x) {
+        x = 200;
+    }
+
+}
