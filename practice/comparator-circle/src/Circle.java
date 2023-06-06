8b2bea1cc4c68827812d6bcf88a706f29afc1102
buihuuhai318 <buihuuhai318@gmail.com>
2023-06-06 14:07:01 +0700

    new

---
+++ practice/comparator-circle/src/Circle.java
@@ -0,0 +1,53 @@
+
+
+public class Circle {
+    public double radius = 1.0;
+    public String color = "blue";
+    public boolean filled = true;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public boolean getFilled() {
+        return filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
