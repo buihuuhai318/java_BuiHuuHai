8a73b7bcc78cdf0e701cba089ab592f22fec61ce
buihuuhai318 <buihuuhai318@gmail.com>
2023-06-07 08:48:43 +0700

    new

--- test/test/src/Main.java
+++ test/test/src/Main.java
@@ -1,19 +1,5 @@
 public class Main {
+    public static void main(String[] args) {
 
-}
-public class A {
-    int a = 5;
-    private int b = 5;
-    public int c = 5;
-    protected d =5;
-} package com.codegym.hanoi
-
-public class B {
-    void method() {
-        A o = new A();
-        o.a = 10;
-        o.b = 10;
-        o.c = 10;
-        o.d = 10;
     }
-}}
+}
