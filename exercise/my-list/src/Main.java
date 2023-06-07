c6e56668f02f53d915d2247caa1689c6082ef171
buihuuhai318 <buihuuhai318@gmail.com>
2023-06-07 22:50:26 +0700

    update

--- exercise/my-list/src/Main.java
+++ exercise/my-list/src/Main.java
@@ -1,26 +1,22 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         MyList<Integer> myList = new MyList<>();
         myList.add(1);
         myList.add(2);
         myList.add(3);
-//        System.out.println(myList.show());
-//        System.out.println(myList.getSize());
+
         myList.addByIndex(1, 4);
-//        System.out.println(myList.get(0));
-//        System.out.println(myList.get(1));
-//        System.out.println(myList.remove(0));
-//        System.out.println(myList.show());
-//        System.out.println(myList.getSize());
+
         myList.remove(1);
-//        System.out.println(myList.show());
-//        System.out.println(myList.getSize());
-        MyList<Integer> a = myList;
-        System.out.println(Arrays.toString(a));
+        System.out.println(myList.show());
 
-
+        myList.add(4);
+        System.out.println(myList.show());
+        myList.ensureCapacity(7);
+        System.out.println(myList.show());
+        System.out.println(myList.getSize());
+        myList.clear();
+        System.out.println(myList.show());
+        System.out.println(myList.getSize());
     }
 }
--- exercise/my-list/src/MyList.java
+++ exercise/my-list/src/MyList.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class MyList<E> {
     private int size = 0;
-    private static final int DEFAULT_CAPACITY = 0;
+    private static final int DEFAULT_CAPACITY = 10;
     private Object[] elements;
 
     public MyList() {
@@ -29,21 +29,14 @@ public class MyList<E> {
         elements[index] = element;
     }
 
-    public void add(E e) {
-        if (size == elements.length) {
-            ensureCapa();
-        }
-        elements[size++] = e;
-    }
-
     public E remove(int index) {
-        E element = (E) elements[index];
+        E newElements = (E) elements[index];
         for (int i = index + 1; i < elements.length; i++) {
             elements[i - 1] = elements[i];
         }
         size--;
-        elements = Arrays.copyOf(elements, elements.length - 1);
-        return element;
+        elements = Arrays.copyOf(elements, elements.length);
+        return newElements;
     }
 
     public E get(int index) {
@@ -61,11 +54,62 @@ public class MyList<E> {
         return string;
     }
 
-    public int getSize() {
-        return size;
+//    public int getSize() {
+//        return size;
+//    }
+//
+////    public Object clone() {
+////        try {
+////            MyList<?> v = (MyList<?>) super.clone();
+////            v.elements = Arrays.copyOf(elements, size);
+////            return v;
+////        } catch (CloneNotSupportedException e) {
+////            // this shouldn't happen, since we are Cloneable
+////            throw new InternalError(e);
+////        }
+//    }
+
+    public boolean contains(E o) {
+        return indexOf(o) >= 0;
     }
 
-    public E clone() {
-        return (E) Arrays.copyOf(elements, elements.length);
+    public int indexOf(E o) {
+        int index = -1;
+        for (int i = 0; i < elements.length; i++) {
+            if (o == elements[i]) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public boolean add(E o) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = o;
+        return true;
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        int elementNull = elements.length - size;
+        if (elementNull < minCapacity) {
+            elements = Arrays.copyOf(elements, elements.length + minCapacity - elementNull);
+        } else {
+            System.out.println("Capacity was ensure!");
+        }
+
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
