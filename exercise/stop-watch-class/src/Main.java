46702f34c0e8fb86bb7f17ed87f360ce2e887d43
buihuuhai318 <buihuuhai318@gmail.com>
2023-06-08 21:35:51 +0700

    up

--- exercise/stop-watch-class/src/Main.java
+++ exercise/stop-watch-class/src/Main.java
@@ -8,20 +8,44 @@ public class Main {
 
         while (true) {
             System.out.println("1. get start time");
-            System.out.println("2. get start millis");
-            System.out.println("3. get end time");
-            System.out.println("4. get end millis");
-            System.out.println("5. get elapsed time");
-            System.out.println("6. exit");
+            System.out.println("2. get end time");
+            System.out.println("3. get elapsed time");
+            System.out.println("4. test loop");
+            System.out.println("5. exit");
             System.out.print("your choice: ");
             choice = input.nextInt();
             switch (choice) {
-                case 1 -> System.out.println("now this star time is: " + test.star());
-                case 2 -> System.out.println("now this star millis time is: " + test.getStartTime());
-                case 3 -> System.out.println("now this end time is: " + test.stop());
-                case 4 -> System.out.println("now this end millis time is: " + test.getEndTime());
-                case 5 -> System.out.println("elapsed time is: " + test.getElapsedTime());
-                case 6 -> System.exit(0);
+                case 1 -> {
+                    test.star();
+                    System.out.println("now this star millis time is: " + test.getStartTime());
+                }
+
+                case 2 -> {
+                    test.stop();
+                    System.out.println("now this end millis time is: " + test.getEndTime());
+                }
+
+                case 3 -> System.out.println("elapsed time is: " + test.getElapsedTime());
+                case 4 -> {
+                    int[] arrNumber = new int[100000];
+                    for (int i = 0; i < arrNumber.length; i++) {
+                        arrNumber[i] = (int) (Math.random()*100000);
+                    }
+                    int box;
+                    test.star();
+                    for (int i = 0; i < arrNumber.length; i++) {
+                        for (int j = i + 1; j < arrNumber.length; j++) {
+                            if (arrNumber[i] < arrNumber[j]) {
+                                box = arrNumber[i];
+                                arrNumber[i] = arrNumber[j];
+                                arrNumber[j] = box;
+                            }
+                        }
+                    }
+                    test.stop();
+                    System.out.println("elapsed time is: " + test.getElapsedTime());
+                }
+                case 5 -> System.exit(0);
                 default -> System.out.println("again");
             }
         }
