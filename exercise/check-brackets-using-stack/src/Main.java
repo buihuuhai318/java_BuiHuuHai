f7a9a9ef6010c6f1620bc62d98a3e535c0e6edcf
buihuuhai318 <buihuuhai318@gmail.com>
2023-06-10 13:58:04 +0700

    new

---
+++ exercise/check-brackets-using-stack/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "s * (s – a) * (s – b) * (s – c)";
+
+        System.out.println(checkBracket(str));
+    }
+
+    public static boolean checkBracket(String str) {
+        Stack<Character> temp = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char x = str.charAt(i);
+            if (x == '(' || x == ')') {
+                if (temp.isEmpty()) {
+                    temp.push(x);
+                } else if (temp.get(0) == '(' && x == ')') {
+                    temp.pop();
+                } else {
+                    temp.push(x);
+                }
+            }
+        }
+        return temp.isEmpty();
+    }
+}
