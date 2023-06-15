462959e67ab2e9d0a2e5d989bfcd8a8b09ea02b8
buihuuhai318 <buihuuhai318@gmail.com>
2023-06-15 21:27:14 +0700

    new

--- exercise/copy-file-binary/src/Main.java
+++ exercise/copy-file-binary/src/Main.java
@@ -8,25 +8,23 @@ public class Main {
         copyFile(source, target);
     }
 
-    public static void copyFile(File source, File target) throws IOException {
+    public static void copyFile(File sourcePath, File targetPath) throws IOException {
         InputStream inputStream = null;
         OutputStream outputStream = null;
 
         try {
-            inputStream = new FileInputStream(source);
-            outputStream = new FileOutputStream(target);
+            inputStream = new FileInputStream(sourcePath);
+            outputStream = new FileOutputStream(targetPath);
 
             byte[] buffer = new byte[1024];
 
-
             int length;
-            while ((length = inputStream.read(buffer)) > 0) {
+            while ((length = inputStream.read()) >0) {
                 outputStream.write(buffer, 0, length);
             }
 
             System.out.println("đã copy file thành công !!!");
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println("file không có hoặc lỗi !!!");
         } finally {
             if (inputStream != null) {
