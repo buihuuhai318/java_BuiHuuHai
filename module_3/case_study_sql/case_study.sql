3b7db38e2a99332472c9ed75dc9389d28f60bbc5
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-07-11 22:55:41 +0700

    new

--- module_3/case_study_sql/case_study.sql
+++ module_3/case_study_sql/case_study.sql
@@ -191,9 +191,9 @@ from khach_hang
 where 
 dia_chi regexp '(Đà Nẵng)|(Quảng Trị)' 
 and 
-((YEAR(CURDATE()) - YEAR(ngay_sinh)) - (RIGHT(CURDATE(), 5) < RIGHT(ngay_sinh, 5))) >= 18 
+((year(curdate()) - year(ngay_sinh)) - (right(curdate(), 5) < right(ngay_sinh, 5))) >= 18 
 and
-((YEAR(CURDATE()) - YEAR(ngay_sinh)) - (RIGHT(CURDATE(), 5) < RIGHT(ngay_sinh, 5))) <= 50;
+((year(curdate()) - year(ngay_sinh)) - (right(curdate(), 5) < right(ngay_sinh, 5))) <= 50;
 
 select ma_khach_hang, ho_ten, ten_loai_khach
 from khach_hang
@@ -215,6 +215,25 @@ where
 loai_khach.ten_loai_khach = "Diamond"
 group by ma_khach_hang
 order by so_lan_dat_phong;
+
+select distinct khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, 
+dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, 
+ifnull((dich_vu_di_kem.gia * hop_dong_chi_tiet.so_luong + dich_vu.chi_phi_thue), 0) as tong_tien
+from khach_hang
+left join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+left join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+left join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem;
+
+
+select distinct dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+join hop_dong on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+where month(ngay_lam_hop_dong) > 4
+and hop_dong.ma_dich_vu not in (select distinct hop_dong.ma_dich_vu from hop_dong where month(ngay_lam_hop_dong) < 4 and year(ngay_lam_hop_dong) = 2020);
+
 
 
 
