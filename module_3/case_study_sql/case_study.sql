a1fc92601baee2ac45b20038670a12f450914035
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-07-12 04:31:40 +0700

    Update case_study.sql

--- module_3/case_study_sql/case_study.sql
+++ module_3/case_study_sql/case_study.sql
@@ -231,8 +231,8 @@ select distinct dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich
 from dich_vu
 join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
 join hop_dong on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
-where month(ngay_lam_hop_dong) > 4
-and hop_dong.ma_dich_vu not in (select distinct hop_dong.ma_dich_vu from hop_dong where month(ngay_lam_hop_dong) < 4 and year(ngay_lam_hop_dong) = 2020);
+where month(ngay_lam_hop_dong) >= 4
+and hop_dong.ma_dich_vu not in (select distinct hop_dong.ma_dich_vu from hop_dong where year(ngay_lam_hop_dong) = 2021 and month(ngay_lam_hop_dong) in (1, 2, 3));
 
 
 
