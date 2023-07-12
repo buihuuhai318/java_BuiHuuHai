6409aed78e1748e206029f1c4af98a7bee285199
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-07-12 16:04:10 +0700

    Update student_management.sql

--- module_3/ss1/exercise/student_management.sql
+++ module_3/ss1/exercise/student_management.sql
@@ -3,27 +3,26 @@ create database student_management;
 use student_management;
 
 create table student (
-	id_student int auto_increment,
+	id_student int primary key auto_increment,
     name_student varchar(60),
     age_student int,
-    country_student varchar(60),
-    primary key (id_student)
+    country_student varchar(60)
 );
 
 create table class (
-	id_class int auto_increment,
-    name_class varchar(60),
-    primary key (id_class)
+	id_class int primary key auto_increment,
+    name_class varchar(60)
 );
 
 create table teacher (
-	id_teacher int auto_increment,
+	id_teacher int primary key auto_increment,
     name_teacher varchar(60),
     age_teacher int,
-    country_teacher varchar(60),
-    primary key (id_teacher)
+    country_teacher varchar(60)
 );
 
-insert into student_management.student (name_student, age_student, country_student) value ("hai", 18, "da nang"), ("thien", 19, "hue");
+insert into student (name_student, age_student, country_student) 
+value ("hai", 18, "da nang"), ("thien", 19, "hue");
 
-select * from student_management.student;
+select * 
+from student;
