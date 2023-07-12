b3118426e2e12cb3344629dec0b8fd3da51ce649
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-07-13 00:03:37 +0700

    Update quan_ly_sinh_vien.sql

--- module_3/ss3/practice/quan_ly_sinh_vien.sql
+++ module_3/ss3/practice/quan_ly_sinh_vien.sql
@@ -65,5 +65,30 @@ insert into mark (subid, studentid, mark, examtimes)
 values (1, 1, 8, 1),
       (1, 2, 10, 2),
       (2, 1, 12, 1);
+      
+      
+select *
+from student;
+
+select *
+from subject
+where credit < 10;
+
+select s.studentid, s.studentname, c.classname
+from student s join class c on s.classid = c.classid;
+
+select s.studentid, s.studentname, c.classname
+from student s join class c on s.classid = c.classid
+where c.classname = 'a1';
+
+select s.studentid, s.studentname, sub.subname, m.mark
+from student s join mark m on s.studentid = m.studentid join subject sub on m.subid = sub.subid;
+
+select s.studentid, s.studentname, sub.subname, m.mark
+from student s join mark m on s.studentid = m.studentid join subject sub on m.subid = sub.subid
+where sub.subname = 'cf';
+
+
+
 
 
