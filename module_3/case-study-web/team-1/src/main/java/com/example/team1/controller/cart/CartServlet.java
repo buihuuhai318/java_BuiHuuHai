a1438944541719592c00a377a8a26b4b61b2d99e
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-08-03 00:54:36 +0700

    new

--- module_3/case-study-web/team-1/src/main/java/com/example/team1/controller/cart/CartServlet.java
+++ module_3/case-study-web/team-1/src/main/java/com/example/team1/controller/cart/CartServlet.java
@@ -33,76 +33,11 @@ public class CartServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        if (session.getAttribute("id_account") != null) {
-            Accounts accounts = accountService.selectAccount((Integer) session.getAttribute("id_account"));
-            int quantity = 1;
-            int id;
-            if (request.getParameter("itemId") != null) {
-                id = Integer.parseInt(request.getParameter("itemId"));
-                Items items = itemService.selectItem(id);
-                if (items != null) {
-                    if (request.getParameter("quantity") != null) {
-                        quantity = Integer.parseInt(request.getParameter("quantity"));
-                    }
-                    if (session.getAttribute("cart") == null) {
-                        Cart cart = new Cart(accounts);
-                        cartService.insertCart(cart);
-                        cart = cartService.selectLastCart();
-                        Map<Integer, OrderDetail> orderDetailMap = new HashMap<>();
-                        OrderDetail orderDetail = new OrderDetail(cart.getId(), items, quantity, items.getPrice());
-                        orderDetailMap.put(id, orderDetail);
-                        orderDetailService.insertOrder(orderDetail);
-                        cart.setDetailList(orderDetailMap);
-                        List<OrderDetail> orderList = new ArrayList<>(cart.getDetailList().values());
-                        session.setAttribute("cart", cart);
-                        session.setAttribute("cartId", cart.getId());
-                        request.setAttribute("orderList", orderList);
-                        String referer = request.getHeader("referer");
-                        if (referer != null) {
-                            response.sendRedirect(referer);
-                        } else {
-                            // Nếu không có trang trước đó, điều hướng về trang chủ hoặc trang giỏ hàng mặc định
-                            request.getRequestDispatcher("shop/cart.jsp").forward(request, response); // hoặc trang giỏ hàng mặc định
-                        }
-                    } else {
-                        Cart cart = (Cart) session.getAttribute("cart");
-                        Map<Integer, OrderDetail> orderDetailMap = cart.getDetailList();
-                        boolean check = false;
-                        for (OrderDetail orderDetail : orderDetailMap.values()) {
-                            if (orderDetail.getItems().getId() == items.getId()) {
-                                orderDetail.setQuantity(orderDetail.getQuantity() + quantity);
-                                orderDetail.setPrice(items.getPrice());
-                                orderDetailService.updateOrder(cart.getId(), orderDetail);
-                                check = true;
-                            }
-                        }
-                        if (!check) {
-                            OrderDetail orderDetail = new OrderDetail(cart.getId(), items, quantity, items.getPrice());
-                            orderDetailMap.put(id, orderDetail);
-                            orderDetailService.insertOrder(orderDetail);
-                        }
-                        session.setAttribute("cart", cart);
-                        session.setAttribute("cartId", cart.getId());
-                        List<OrderDetail> orderList = new ArrayList<>(cart.getDetailList().values());
-                        request.setAttribute("orderList", orderList);
-                        String referer = request.getHeader("referer");
-                        if (referer != null) {
-                            response.sendRedirect(referer);
-                        } else {
-                            // Nếu không có trang trước đó, điều hướng về trang chủ hoặc trang giỏ hàng mặc định
-                            request.getRequestDispatcher("shop/cart.jsp").forward(request, response); // hoặc trang giỏ hàng mặc định
-                        }
-                    }
-                }
-            }
-        } else {
-            response.sendRedirect("/shop/login.jsp");
-        }
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+
     }
 }
--- module_3/case-study-web/team-1/src/main/java/com/example/team1/controller/employee/EmployeeServlet.java
+++ module_3/case-study-web/team-1/src/main/java/com/example/team1/controller/employee/EmployeeServlet.java
@@ -21,119 +21,11 @@ public class EmployeeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getParameter("action");
-        if (action == null) {
-            action = "";
-        }
-        switch (action) {
-            case "edit":
-                showEdit(request, response);
-                break;
-            case "list":
-                showList(request, response);
-                break;
-            case "editList":
-                showEditList(request, response);
-            case "delete":
-                delete(request, response);
-                break;
-            default:
-                showInfo(request, response);
-                break;
-        }
-    }
-
-    private void showInfo(HttpServletRequest request, HttpServletResponse response) {
-
-    }
-
-    private void delete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
-        Employees employees = employeeService.selectEmployee(id);
-        Accounts accounts = accountService.selectAllAccountByEmail().get(employees.getEmail());
-
-        if (accounts.getRole().getId() != Accounts.ADNIN) {
-            accountService.deleteAccount(accounts.getId(), true);
-            employeeService.deleteEmployee(id, true);
-        }
-        response.sendRedirect("EmployeeServlet?action=list");
-    }
-
-    private void showEditList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
-        Employees employees = employeeService.selectEmployee(id);
-        request.setAttribute("employees", employees);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("admin/edit-profile-employee.jsp");
-        dispatcher.forward(request, response);
-    }
-
-    private void showList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Employees> employeesList = new ArrayList<>(employeeService.selectAllEmployee().values());
-        request.setAttribute("employeesList", employeesList);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("admin/employee-list.jsp");
-        dispatcher.forward(request, response);
-    }
-
-    private void showEdit(HttpServletRequest request, HttpServletResponse response) {
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getParameter("action");
-        if (action == null) {
-            action = "";
-        }
-        switch (action) {
-            case "create":
-                create(request, response);
-                break;
-            case "edit":
-                edit(request, response);
-                break;
-            case "editList":
-                editList(request, response);
-                break;
-        }
-    }
-
-    private void editList(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
-        String name = request.getParameter("name");
-        String address = request.getParameter("address");
-        String birthday = request.getParameter("birthday");
-        int gender = Integer.parseInt(request.getParameter("gender"));
-        int salary = Integer.parseInt(request.getParameter("salary"));
-        int available = Integer.parseInt(request.getParameter("available"));
-        String phone = request.getParameter("phone");
-        String image = request.getParameter("image");
-
-        Employees employees = employeeService.selectEmployee(id);
-        Accounts accounts = accountService.selectAllAccountByEmail().get(employees.getEmail());
-
-        if (accounts.getRole().getId() != Accounts.ADNIN) {
-            accountService.deleteAccount(accounts.getId(), available == 1);
-            employees.setId(id);
-            employees.setSalary(salary);
-            employees.setName(name);
-            employees.setAddress(address);
-            employees.setBirthday(birthday);
-            employees.setGender(gender);
-            employees.setStatus(available);
-            employees.setPhone(phone);
-            if (!image.equals("")) {
-                employees.setImage(image);
-            }
-            employeeService.updateEmployee(employees.getId(), employees);
-        }
-        response.sendRedirect("EmployeeServlet?action=list");
-    }
-
-    private void edit(HttpServletRequest request, HttpServletResponse response) {
-
-    }
-
-    private void create(HttpServletRequest request, HttpServletResponse response) {
 
     }
 }
